7aa991941fee6e330595af8f22206da595529f22
Jrm1992 <jose_roberto_6@msn.com>
2023-06-29 03:59:54 -0300

    created newsletter modal component on submit

--- src/app/components/newsletter/newsletter.component.ts
+++ src/app/components/newsletter/newsletter.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-newsletter',
   templateUrl: './newsletter.component.html',
-  styleUrls: ['./newsletter.component.scss']
+  styleUrls: ['./newsletter.component.scss'],
 })
 export class NewsletterComponent {
+  form!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  get f() {
+    return this.form.get('email');
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    document.querySelector('.modal-container')?.classList.remove('hidden');
+    this.reset();
+  }
+
+  reset() {
+    this.form.reset();
+  }
 }
